f38216b6c22a405d536e5aa17b3389a8d8d20564
Tri Nguyen <34323747+tri97nguyen@users.noreply.github.com>
2026-05-07 23:06:12 -0400

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import System.IO (hFlush, stdout)
+import Control.Monad (when)
 
 main :: IO ()
 main = do
@@ -11,5 +12,6 @@ repl = do
     putStr "$ "
     hFlush stdout
     input <- getLine
+    when (input == "exit") $ return ()
     putStrLn $ input ++ ": command not found"
     repl
