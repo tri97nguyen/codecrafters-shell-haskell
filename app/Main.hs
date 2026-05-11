958195ce57ecb56e002da32281eaac7df19e1736
Tri Nguyen <34323747+tri97nguyen@users.noreply.github.com>
2026-05-11 00:44:57 -0400

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,41 @@ module Main (main) where
 
 import System.IO (hFlush, stdout)
 import Control.Monad (when)
-import Control.Monad.State
+import Control.Monad.State (State, get, put, runState, execState)
 import System.Environment (getEnv)
+import Data.Sequence (Seq((:<|), (:|>), Empty), (|>), singleton)
+import Data.Foldable (toList)
+
+splitOn :: String -> Char -> [String]
+splitOn text delimiter =
+    toList $ execState (splitString text) (singleton "")
+
+    where
+    splitString :: String -> State (Seq String) ()
+    splitString "" = return ()
+    splitString (char : tail) = do
+        parseChar char
+        splitString tail
+
+
+    parseChar :: Char -> State (Seq String) ()
+    parseChar char = do
+        currentState <- get
+        if char == delimiter then put $ currentState |> ""
+        else do
+            let (front :|> lastItem) = currentState
+                newLastItem = lastItem <> [char]
+            put (front :|> newLastItem)
+
+mysplit :: Char -> String -> [String]
+mysplit delimiter text =
+    toList $ foldl accumulatorFn (singleton "") text
+    where
+        accumulatorFn :: Seq String -> Char -> Seq String
+        accumulatorFn (front :|> end) next
+            | next == delimiter = front |> end |> ""
+            | otherwise = front |> (end ++ [next])
+        
 
 main :: IO ()
 main = do
@@ -16,28 +49,16 @@ typeCommand (command:otherCommands)
     | command `elem` builtInCommands = return $ command <> " is a shell builtin"
     | otherwise = return $ command <> ": not found"
 
-splitOn :: String -> Char -> [String]
-splitOn word delimiter = 
-    undefined
-    where
-        delimiter = ","
-        collectTillDelimiter :: String -> [String] -> [String]
-        collectTillDelimiter text result = undefined
 
 
--- delimiter = ","
--- collectTillDelimiter :: String -> a
--- collectTillDelimiter text =
---     let a = foldl (\acc nextChar -> ) [] text
---     in _
-    
+
 
 checkCommandInPath :: IO String
 checkCommandInPath = do
     path <- getEnv "PATH"
 
     return ""
-    
+
 
 
 repl :: IO ()
