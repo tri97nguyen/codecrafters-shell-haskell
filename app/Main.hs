87443c22d8e487b23c1e6a50f1824d85be66cec4
Tri Nguyen <34323747+tri97nguyen@users.noreply.github.com>
2026-05-22 00:51:41 -0400

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -3,31 +3,59 @@ module Main (main) where
 import System.IO (hFlush, stdout)
 import Command.Type (typeCommand, isExternalCommand)
 import System.Process (readProcess)
-import System.Directory (getCurrentDirectory)
+import System.Directory (getCurrentDirectory, doesDirectoryExist)
+import Control.Monad.Reader (ReaderT (runReaderT), runReaderT, lift, ask)
+import Data.IORef (IORef, newIORef, readIORef, modifyIORef, writeIORef)
+
+data AppState = AppState {
+  currentWorkingDirectory :: IORef FilePath
+}
+
+data Env = Env {
+  appState :: AppState
+}
+
+type App a = ReaderT Env IO a
 
 main :: IO ()
 main = do
-  repl
+  curDir <- getCurrentDirectory >>= newIORef
+  let env = Env {appState = AppState {currentWorkingDirectory = curDir}}
+  runReaderT repl env
+  
 
 
-repl :: IO ()
+repl :: App ()
 repl = do
-  putStr "$ "
-  hFlush stdout
-  input <- getLine
-  if input == "exit"
-    then return ()
-    else do
-      case words input of
-        (cmd : args) -> do
-          case cmd of
-            "type" -> typeCommand args >>= mapM_ putStrLn
-            "echo" -> putStrLn $ unwords args
-            "pwd" -> getCurrentDirectory >>= putStrLn
-            _ -> do
-              mFilePath <- isExternalCommand cmd
-              case mFilePath of
-                Just _ -> do readProcess cmd args "" >>= putStr
-                Nothing -> putStrLn $ input ++ ": command not found"
-        _ -> return ()
-      repl
+  env <- ask -- is this clean? Earlier repl is of type IO (), but after introducing readerT, I have converted it to App (), and so I have to do the lifting
+  lift $ do
+    putStr "$ "
+    hFlush stdout
+    input <- getLine
+    if input == "exit"
+      then return ()
+      else do
+        case words input of
+          (cmd : args) -> do
+            case cmd of
+              "type" -> typeCommand args >>= mapM_ putStrLn
+              "echo" -> putStrLn $ unwords args
+              "pwd" -> readIORef (currentWorkingDirectory . appState $ env) >>= putStrLn
+              "cd" -> do
+                case args of
+                  (path : _) -> do
+                    let dirRef = currentWorkingDirectory . appState $ env
+                    dirExist <- doesDirectoryExist path
+                    if dirExist then
+                      writeIORef dirRef path
+                    else
+                      putStrLn $ "cd: " ++ path ++ ": No such file or directory"
+                  _ -> return ()
+
+              _ -> do
+                mFilePath <- isExternalCommand cmd
+                case mFilePath of
+                  Just _ -> do readProcess cmd args "" >>= putStr
+                  Nothing -> putStrLn $ input ++ ": command not found"
+          _ -> return ()
+        runReaderT repl env
